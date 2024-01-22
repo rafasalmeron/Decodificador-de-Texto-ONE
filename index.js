75044ea86a638fa618f627acef962b7a88c3e166
Salmeron <rafasalmeronjr@gmail.com>
2024-01-22 17:20:00 -0300

    Esqueleto

---
+++ index.js
@@ -0,0 +1,8 @@
+var textarea = document.getElementById('auto-resize-textarea');
+
+    // Adiciona um ouvinte de eventos para monitorar as mudanças de conteúdo
+    textarea.addEventListener('input', function () {
+      // Define a altura da textarea baseada no conteúdo
+      this.style.height = 'auto';
+      this.style.height = (this.scrollHeight + 2) + 'px';
+    });
