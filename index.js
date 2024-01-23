ec560ae4e5e083889f7386ab6053a3bb82630311
Salmeron <rafasalmeronjr@gmail.com>
2024-01-23 04:28:07 -0300

    Fim da Estilização(for a while)

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-var textarea = document.getElementById('auto-resize-textarea');
+var textarea = document.getElementById("auto-resize-textarea");
 
-    // Adiciona um ouvinte de eventos para monitorar as mudanças de conteúdo
-    textarea.addEventListener('input', function () {
-      // Define a altura da textarea baseada no conteúdo
-      this.style.height = 'auto';
-      this.style.height = (this.scrollHeight + 2) + 'px';
-    });
+// Adiciona um ouvinte de eventos para monitorar as mudanças de conteúdo
+textarea.addEventListener("input", function () {
+  // Define a altura da textarea baseada no conteúdo
+  this.style.height = "auto";
+  this.style.height = this.scrollHeight + 2 + "px";
+});
